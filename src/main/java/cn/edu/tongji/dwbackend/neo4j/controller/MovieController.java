d6a1e7b7da5489853206c5fc2f1f23f070e84d93
汪汪汪 <1209497940@qq.com>
2021-12-06 19:20:14 +0800

    修改neo4j mapping

--- src/main/java/cn/edu/tongji/dwbackend/neo4j/controller/MovieController.java
+++ src/main/java/cn/edu/tongji/dwbackend/neo4j/controller/MovieController.java
@@ -1,10 +1,12 @@
 package cn.edu.tongji.dwbackend.neo4j.controller;
 
 import org.neo4j.driver.Driver;
+import org.neo4j.driver.Record;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,7 @@ import java.util.Map;
  */
 
 @RestController
+@RequestMapping("/neo4j/movie")
 public class MovieController {
     private final Driver driver;
 
@@ -28,87 +31,132 @@ public class MovieController {
         this.driver = driver;
     }
 
-    @GetMapping(path = "/movies", produces = MediaType.APPLICATION_JSON_VALUE)
-    public HashMap<String,Object> getMovieByCondition(@RequestParam(required = false) Integer year,
-                                                 @RequestParam(required = false) Integer month,
-                                                 @RequestParam(required = false) Integer day,
-                                                 @RequestParam(required = false) Integer quarter,
-                                                 @RequestParam(required = false) String movieName) {
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public HashMap<String,Object> getMovieByCondition(@RequestParam(required = false) String movieName,
+                                                      @RequestParam(required = false) String category,
+                                                      @RequestParam(required = false) List<String> directorNames,
+                                                      @RequestParam(required = false) List<String> mainActors,
+                                                      @RequestParam(required = false) List<String> actors,
+                                                      @RequestParam(required = false) String minScore,
+                                                      @RequestParam(required = false) String maxScore,
+                                                      @RequestParam(required = false) Integer minYear,
+                                                      @RequestParam(required = false) Integer minMonth,
+                                                      @RequestParam(required = false) Integer minDay,
+                                                      @RequestParam(required = false) Integer maxYear,
+                                                      @RequestParam(required = false) Integer maxMonth,
+                                                      @RequestParam(required = false) Integer maxDay) {
+
+        // match (m:Movie), (m)-[:Belong]->(c:Category{name:'DTS'}) where m.title = 'Book and Sword' return count(m)
+        // where m.year*10000+m.month*100+m.day >=20101102
+        // 导演：match (m:Movie), (m)<-[:MainAct]-(:Person{name:'Santana'}),(m)<-[:MainAct]-(:Person{name:'Treglia'})
+        // 评分用 where
 
         try (Session session = driver.session()) {
-            String searchCondition = "MATCH (n:Movie) ";
+            String query = "match (m:Movie) ";
+            if (category != null){
+                query +=" , (m)-[:Belong]->(:Category{name:'"+category+"'}) ";
+            }
 
-            boolean firstCondition=true;
-            if(year!=null){
-                searchCondition+=" where ";
-                searchCondition+=" n.year="+year;
-                firstCondition=false;
+            // 导演名称
+            if(directorNames != null){
+                for(String directorName: directorNames){
+                    query += " ,(m)<-[:Direct]-(:Person{name;'"+directorName+"'})";
+                }
             }
-            if(month!=null){
-                if(!firstCondition){
-                    searchCondition+=" and ";
-                }
-                else{
-                    searchCondition+=" where ";
-                }
-                searchCondition+= " n.month="+month;
-                firstCondition=false;
-            }
-            if(day!=null){
-                if(!firstCondition){
-                    searchCondition+=" and ";
-                }
-                else{
-                    searchCondition+=" where ";
-                }
-                searchCondition+= " n.day="+day;
-                firstCondition=false;
-            }
-            if(quarter!=null){
-                if(!firstCondition){
-                    searchCondition+=" and ";
-                }
-                else{
-                    searchCondition+=" where ";
-                }
-                int minMonth = quarter*3-2;
-                int maxMonth = quarter*3;
-                searchCondition+= " n.month>="+minMonth+" and n.month<="+maxMonth;
-                firstCondition=false;
-            }
-            if(movieName!=null){
-                if(!firstCondition){
-                    searchCondition+=" and ";
-                }
-                else{
-                    searchCondition+=" where ";
-                }
-                searchCondition+= " n.name contains "+movieName;
-                firstCondition=false;
-            }
-            searchCondition+=" return count(n)";
 
+            // 主演名称
+            if(mainActors != null){
+                for(String mainActor: mainActors){
+                    query += " ,(m)<-[:MainAct]-(:Person{name;'"+mainActor+"'})";
+                }
+            }
+
+            // 演员名称
+            if(actors != null){
+                for(String actor: actors){
+                    query += " ,(m)<-[:Act]-(:Person{name;'"+actor+"'})";
+                }
+            }
+
+            Boolean whereAppear = false;
+            // 电影名称
+            if(movieName != null){
+                query += " where m.title='"+movieName+"' ";
+                whereAppear = true;
+            }
+
+            // 最低评分
+            if (minScore != null){
+                if (whereAppear){
+                    query+= " and ";
+                }
+                else {
+                    query += " where ";
+                    whereAppear = true;
+                }
+                query += " m.score >="+ minScore+" ";
+            }
+
+            // 最高评分
+            if (maxScore != null){
+                if (whereAppear){
+                    query+= " and ";
+                }
+                else {
+                    query += " where ";
+                    whereAppear = true;
+                }
+                query+=" m.score <= "+maxScore+" ";
+            }
+
+
+            // 上映时间
+            if(minYear!=null){
+                if (whereAppear){
+                    query+= " and ";
+                }
+                else {
+                    query += " where ";
+                    whereAppear = true;
+                }
+                query+=" m.year*10000+m.month*100+m.day >= "+(10000*minYear+100*minMonth+minDay)+" ";
+            }
+            if(maxYear!=null){
+                if (whereAppear){
+                    query+= " and ";
+                }
+                else {
+                    query += " where ";
+                    whereAppear = true;
+                }
+                query+=" m.year*10000+m.month*100+m.day <= "+(10000*maxYear+100*maxMonth+maxDay)+" ";
+            }
+
+            query+=" return m.title ";
+            System.out.println("查询语句为: "+query);
 
             // 记录开始时间
             long startTime = System.currentTimeMillis();
-
             Result res=
-                session.run(searchCondition);
+                    session.run(query);
 
             // 记录结束时间
             long endTime = System.currentTimeMillis();
 
 
             HashMap<String,Object> response = new HashMap<>();
-            response.put("number",res
-                    .list().get(0).get(0).toString());
+
+            List<Record> result = res.list();
+
+            response.put("movies",res
+                    .list(r -> r.get(0).asString()));
             response.put("time",endTime-startTime);
 
             return response;
         }
     }
 
-    @GetMapping(path = "movies/name", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/name", produces = MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, Object> findMovieByName(@RequestParam String name){
         try (Session session = driver.session()) {
             // 记录开始时间
@@ -129,7 +177,7 @@ public class MovieController {
         }
     }
 
-    @GetMapping(path = "movies/director",produces =  MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/director",produces =  MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, Object> findMovieByDirectorName(@RequestParam String name){
         //
         try (Session session = driver.session()) {
@@ -151,7 +199,7 @@ public class MovieController {
         }
     }
 
-    @GetMapping(path = "movies/mainactor",produces =  MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/mainactor",produces =  MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, Object> findMovieByMainActorName(@RequestParam String name){
         //
         try (Session session = driver.session()) {
@@ -173,7 +221,7 @@ public class MovieController {
         }
     }
 
-    @GetMapping(path = "movies/actor",produces =  MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/actor",produces =  MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, Object> findMovieByActorName(@RequestParam String name){
         //
         try (Session session = driver.session()) {
@@ -194,6 +242,7 @@ public class MovieController {
             return response;
         }
     }
+
 
 
 
