05452d170e779a46178c6fda0d16df4c6e945f18
Qiao Liang <19921305202@163.com>
2021-12-06 19:05:47 +0800

    finish the configuration of relational database

---
+++ src/main/java/cn/edu/tongji/dwbackend/Mysql/Controller/RelationshipController.java
@@ -0,0 +1,16 @@
+package
+        cn.edu.tongji.dwbackend.Mysql.Controller;/**
+ * @author 梁乔 2021/12/6
+ **/
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 此处写RelationshipController类的描述
+ * @author 梁乔
+ * @since 2021/12/6 14:01 
+ */
+@RestController
+public class RelationshipController {
+
+}
