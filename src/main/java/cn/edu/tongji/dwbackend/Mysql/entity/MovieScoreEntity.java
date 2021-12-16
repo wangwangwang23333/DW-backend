71634b756c4eced22a2e99749f59088a63725af7
Qiao Liang <19921305202@163.com>
2021-12-16 14:44:33 +0800

    更新movie_score表

--- src/main/java/cn/edu/tongji/dwbackend/Mysql/entity/MovieScoreEntity.java
+++ src/main/java/cn/edu/tongji/dwbackend/Mysql/entity/MovieScoreEntity.java
@@ -1,6 +1,6 @@
 package
         cn.edu.tongji.dwbackend.Mysql.entity;/**
- * @author 梁乔 2021/12/11
+ * @author 梁乔 2021/12/16
  **/
 
 import javax.persistence.*;
@@ -8,13 +8,15 @@ import javax.persistence.*;
 /**
  * 此处写MovieScoreEntity类的描述
  * @author 梁乔
- * @since 2021/12/11 15:56 
+ * @since 2021/12/16 14:43 
  */
 @Entity
 @Table(name = "movie_score", schema = "DataWarehouse", catalog = "")
 public class MovieScoreEntity {
     private int movieId;
     private Double movieScore;
+    private Double positiveCommentRating;
+    private Double negativeCommentRating;
 
     @Id
     @Column(name = "movie_id")
@@ -36,6 +38,26 @@ public class MovieScoreEntity {
         this.movieScore = movieScore;
     }
 
+    @Basic
+    @Column(name = "positive_comment_rating")
+    public Double getPositiveCommentRating() {
+        return positiveCommentRating;
+    }
+
+    public void setPositiveCommentRating(Double positiveCommentRating) {
+        this.positiveCommentRating = positiveCommentRating;
+    }
+
+    @Basic
+    @Column(name = "negative_comment_rating")
+    public Double getNegativeCommentRating() {
+        return negativeCommentRating;
+    }
+
+    public void setNegativeCommentRating(Double negativeCommentRating) {
+        this.negativeCommentRating = negativeCommentRating;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -45,6 +67,10 @@ public class MovieScoreEntity {
 
         if (movieId != that.movieId) return false;
         if (movieScore != null ? !movieScore.equals(that.movieScore) : that.movieScore != null) return false;
+        if (positiveCommentRating != null ? !positiveCommentRating.equals(that.positiveCommentRating) : that.positiveCommentRating != null)
+            return false;
+        if (negativeCommentRating != null ? !negativeCommentRating.equals(that.negativeCommentRating) : that.negativeCommentRating != null)
+            return false;
 
         return true;
     }
@@ -53,6 +79,8 @@ public class MovieScoreEntity {
     public int hashCode() {
         int result = movieId;
         result = 31 * result + (movieScore != null ? movieScore.hashCode() : 0);
+        result = 31 * result + (positiveCommentRating != null ? positiveCommentRating.hashCode() : 0);
+        result = 31 * result + (negativeCommentRating != null ? negativeCommentRating.hashCode() : 0);
         return result;
     }
 }
